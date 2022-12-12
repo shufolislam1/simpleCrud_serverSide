5c9119cc315bf4b6386998e93f4d27eb29b43671
shufol islam <shufolislam1255@gmail.com>
2022-12-12 18:00:49 +0600

    post route done and solved a problem

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const port = 5000;
+const router = express.Router();
 
 const app = express();
 
+// middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 // connexting mongoose
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1:27017/product')
 .then(() => {console.log('Database connected successfully')})
 .catch(error => console.log(error))
 
 // routes
-
+app.post('/addItem', async (req, res) => { //routes er bodol e app. use kora lagse. route use korle kaj kore na
+    const addnewItem = await req.body;
+    console.log(addnewItem);
+    res.send({message: "success"})
+})
+app.get('/',  (req,res)=> {
+     res.send({message: "success"});
+    // console.log('something');
+})
 
 // app listen
 app.listen(port, () =>{
